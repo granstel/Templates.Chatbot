9175f1b30bd8c4a96e6b74111b6cb4462334cd9d
Stepan Grankin <sgrankin@inbox.ru>
2019-12-02 09:24:51 +0300

    map QueryResult of DetectIntentResponse to internal Dialog type

--- src/GranSteL.Chatbot.Models/Internal/Dialog.cs
+++ src/GranSteL.Chatbot.Models/Internal/Dialog.cs
@@ -1,6 +1,15 @@
-﻿namespace GranSteL.Chatbot.Models.Internal
+﻿using System.Collections.Generic;
+
+namespace GranSteL.Chatbot.Models.Internal
 {
     public class Dialog
     {
+        public IDictionary<string, string> Parameters { get; set; }
+
+        public bool EndConversation { get; set; }
+
+        public bool ParametersIncomplete { get; set; }
+
+        public string Response { get; set; }
     }
 }
---
+++ src/GranSteL.Chatbot.Services/Mapping/DialogflowProfile.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Google.Cloud.Dialogflow.V2;
+using Google.Protobuf.WellKnownTypes;
+using GranSteL.Chatbot.Models.Internal;
+
+namespace GranSteL.Chatbot.Services.Mapping
+{
+    public class DialogflowProfile : Profile
+    {
+        public DialogflowProfile()
+        {
+            CreateMap<QueryResult, Dialog>()
+                .ForMember(d => d.EndConversation, m => m.MapFrom(s => s.Intent.ResetContexts))
+                .ForMember(d => d.Parameters, m => m.MapFrom(s => GetParameters(s)))
+                .ForMember(d => d.Response, m => m.MapFrom(s => s.FulfillmentText))
+                .ForMember(d => d.ParametersIncomplete, m => m.MapFrom(s => !s.AllRequiredParamsPresent));
+        }
+
+        private IDictionary<string, string> GetParameters(QueryResult queryResult)
+        {
+            var dictionary = new Dictionary<string, string>();
+
+            var fields = queryResult?.Parameters.Fields;
+
+            if (fields?.Any() != true)
+            {
+                return dictionary;
+            }
+
+            foreach (var field in fields)
+            {
+                if (field.Value.KindCase == Value.KindOneofCase.StringValue)
+                {
+                    dictionary.Add(field.Key, field.Value.StringValue);
+                }
+                else if (field.Value.KindCase == Value.KindOneofCase.StructValue)
+                {
+                    var stringValues = new List<string>();
+
+                    foreach (var valueField in field.Value.StructValue.Fields)
+                    {
+                        if (valueField.Value.KindCase == Value.KindOneofCase.StringValue)
+                        {
+                            stringValues.Add(valueField.Value.StringValue);
+                        }
+                    }
+
+                    var stringValue = string.Join("/", stringValues);
+
+                    dictionary.Add(field.Key, stringValue);
+                }
+            }
+
+            return dictionary;
+        }
+    }
+}
