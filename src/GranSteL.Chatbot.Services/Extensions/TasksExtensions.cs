3541283861d6071b8ec05b5871c2aff9e68c44af
Stepan Grankin <sgrankin@inbox.ru>
2020-03-13 09:57:58 +0300

    Add extensions for Task to fire-and-forget

---
+++ src/GranSteL.Chatbot.Services/Extensions/TasksExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using NLog;
+
+namespace GranSteL.Chatbot.Services.Extensions
+{
+    public static class TasksExtensions
+    {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Fire-and-forget
+        /// Позволяет не дожидаться завершения задачи.
+        /// В случае ошибки исключение будет логировано.
+        /// </summary>
+        /// <param name="task"></param>
+        public static void Forget(this Task task)
+        {
+            Task.Factory.StartNew(async () =>
+            {
+                try
+                {
+                    await task;
+                }
+                catch (Exception e)
+                {
+                    Log.Error(e, "Error while executing the task");
+                }
+            });
+        }
+    }
+}
