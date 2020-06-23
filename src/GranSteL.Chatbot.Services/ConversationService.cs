d89f8fdce5daafd8f1de813d574b3cd3fe825ab3
Stepan Grankin <sgrankin@inbox.ru>
2020-06-23 20:53:35 +0300

    Add exampe of getting response from DF

--- src/GranSteL.Chatbot.Services/ConversationService.cs
+++ src/GranSteL.Chatbot.Services/ConversationService.cs
@@ -5,10 +5,21 @@ namespace GranSteL.Chatbot.Services
 {
     public class ConversationService : IConversationService
     {
+        private readonly IDialogflowService _dialogflowService;
+
+        public ConversationService(IDialogflowService dialogflowService)
+        {
+            _dialogflowService = dialogflowService;
+        }
+
         public async Task<Response> GetResponseAsync(Request request)
         {
             //TODO: processing commands, invoking external services, and other cool asynchronous staff to generate response
-            return await Task.Run(() => default(Response));
+            var dialog = await _dialogflowService.GetResponseAsync(request);
+
+            var response = new Response { Text = dialog.Response, Finished = dialog.EndConversation };
+
+            return response;
         }
     }
 }
