49049c42be25bac04a2eb0f843b41214af21bc57
Stepan Grankin <sgrankin@inbox.ru>
2020-06-26 20:01:57 +0300

    Add messengers' project and test only by parameters (-t, -y, or -chat2desc)

--- src/AddChat2Desk.cmd
+++ src/AddChat2Desk.cmd
@@ -1,1 +1,3 @@
+dotnet sln add GranSteL.Chatbot.Messengers.Chat2Desk/GranSteL.Chatbot.Messengers.Chat2Desk.csproj
+dotnet sln add GranSteL.Chatbot.Messengers.Chat2Desk.Tests/GranSteL.Chatbot.Messengers.Chat2Desk.Tests.csproj
 dotnet add GranSteL.Chatbot.Api/GranSteL.Chatbot.Api.csproj reference GranSteL.Chatbot.Messengers.Chat2Desk/GranSteL.Chatbot.Messengers.Chat2Desk.csproj
--- src/AddTelegram.cmd
+++ src/AddTelegram.cmd
@@ -1,1 +1,3 @@
+dotnet sln add GranSteL.Chatbot.Messengers.Telegram/GranSteL.Chatbot.Messengers.Telegram.csproj
+dotnet sln add GranSteL.Chatbot.Messengers.Telegram.Tests/GranSteL.Chatbot.Messengers.Telegram.Tests.csproj
 dotnet add GranSteL.Chatbot.Api/GranSteL.Chatbot.Api.csproj reference GranSteL.Chatbot.Messengers.Telegram/GranSteL.Chatbot.Messengers.Telegram.csproj
