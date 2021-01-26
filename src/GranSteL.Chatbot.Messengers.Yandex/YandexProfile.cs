e7db3088069015fd983c3c3a4f457bf1a82e55ab
Stepan Grankin <sgrankin@inbox.ru>
2021-01-26 21:26:09 +0300

    fix yandex mapping profile

--- src/GranSteL.Chatbot.Messengers.Yandex/YandexProfile.cs
+++ src/GranSteL.Chatbot.Messengers.Yandex/YandexProfile.cs
@@ -2,9 +2,8 @@ using System;
 using AutoMapper;
 using GranSteL.Chatbot.Models.Internal;
 using Yandex.Dialogs.Models;
-using Yandex.Dialogs.Models.Cards;
 using Yandex.Dialogs.Models.Input;
-using Internal = GranSteL.Chatbot.Models.Internal;
+using InternalModels = GranSteL.Chatbot.Models.Internal;
 using YandexModels = Yandex.Dialogs.Models;
 
 namespace GranSteL.Chatbot.Messengers.Yandex
@@ -16,16 +15,16 @@ namespace GranSteL.Chatbot.Messengers.Yandex
     {
         public YandexProfile()
         {
-            CreateMap<InputModel, Internal.Request>()
-                .ForMember(d => d.ChatHash, m => m.ResolveUsing(s => s.Session?.SkillId))
-                .ForMember(d => d.UserHash, m => m.ResolveUsing(s => s.Session?.UserId))
-                .ForMember(d => d.Text, m => m.ResolveUsing(s => s.Request?.OriginalUtterance))
-                .ForMember(d => d.SessionId, m => m.ResolveUsing(s => s.Session?.SessionId))
-                .ForMember(d => d.NewSession, m => m.ResolveUsing(s => s.Session?.New))
-                .ForMember(d => d.Language, m => m.ResolveUsing(s => s.Meta?.Locale))
-                .ForMember(d => d.Source, m => m.UseValue(Source.Yandex));
+            CreateMap<InputModel, InternalModels.Request>()
+                .ForMember(d => d.ChatHash, m => m.MapFrom((s, d) => s.Session?.SkillId))
+                .ForMember(d => d.UserHash, m => m.MapFrom((s, d) => s.Session?.UserId))
+                .ForMember(d => d.Text, m => m.MapFrom((s, d) => s.Request?.OriginalUtterance))
+                .ForMember(d => d.SessionId, m => m.MapFrom((s, d) => s.Session?.SessionId))
+                .ForMember(d => d.NewSession, m => m.MapFrom((s, d) => s.Session?.New))
+                .ForMember(d => d.Language, m => m.MapFrom((s, d) => s.Meta?.Locale))
+                .ForMember(d => d.Source, m => m.MapFrom(s => Source.Yandex));
 
-            CreateMap<Internal.Response, OutputModel>()
+            CreateMap<InternalModels.Response, OutputModel>()
                 .ForMember(d => d.Response, m => m.MapFrom(s => s))
                 .ForMember(d => d.Session, m => m.MapFrom(s => s))
                 .ForMember(d => d.Version, m => m.Ignore())
@@ -33,14 +32,14 @@ namespace GranSteL.Chatbot.Messengers.Yandex
                 .ForMember(d => d.UserStateUpdate, m => m.Ignore())
                 .ForMember(d => d.SessionState, m => m.Ignore());
 
-            CreateMap<Internal.Response, YandexModels.Response>()
+            CreateMap<InternalModels.Response, YandexModels.Response>()
                 .ForMember(d => d.Text, m => m.MapFrom(s => s.Text.Replace(Environment.NewLine, "\n")))
                 .ForMember(d => d.Tts, m => m.MapFrom(s => s.AlternativeText.Replace(Environment.NewLine, "\n")))
                 .ForMember(d => d.EndSession, m => m.MapFrom(s => s.Finished))
                 .ForMember(d => d.Card, m => m.Ignore())
                 .ForMember(d => d.Buttons, m => m.Ignore());
 
-            CreateMap<Internal.Response, Session>()
+            CreateMap<InternalModels.Response, Session>()
                 .ForMember(d => d.UserId, m => m.MapFrom(s => s.UserHash))
                 .ForMember(d => d.MessageId, m => m.Ignore())
                 .ForMember(d => d.SessionId, m => m.Ignore())
