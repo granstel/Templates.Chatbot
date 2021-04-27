080c29468628b6fd8fc43e579b30bb4e5c3d18d6
Stepan Grankin <sgrankin@inbox.ru>
2021-04-27 20:30:28 +0300

    script for adding Marusia

---
+++ src/AddMarusia.cmd
@@ -0,0 +1,3 @@
+dotnet sln add GranSteL.Chatbot.Messengers.Marusia/GranSteL.Chatbot.Messengers.Marusia.csproj -s Messengers
+dotnet sln add GranSteL.Chatbot.Messengers.Marusia.Tests/GranSteL.Chatbot.Messengers.Marusia.Tests.csproj -s Tests
+dotnet add GranSteL.Chatbot.Api/GranSteL.Chatbot.Api.csproj reference GranSteL.Chatbot.Messengers.Marusia/GranSteL.Chatbot.Messengers.Marusia.csproj
