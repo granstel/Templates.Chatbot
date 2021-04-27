6397621e739449f6b3452d2dcab5193d2f63e8ca
Stepan Grankin <sgrankin@inbox.ru>
2021-04-27 20:30:45 +0300

    files with tests for Marusia

---
+++ src/GranSteL.Chatbot.Messengers.Marusia.Tests/MarusiaProfileTests.cs
@@ -0,0 +1,55 @@
+﻿using AutoFixture;
+using AutoFixture.Kernel;
+using AutoMapper;
+using MailRu.Marusia.Models;
+using MailRu.Marusia.Models.Buttons;
+using MailRu.Marusia.Models.Cards;
+using MailRu.Marusia.Models.Input;
+using NUnit.Framework;
+
+namespace GranSteL.Chatbot.Messengers.Marusia.Tests
+{
+    [TestFixture]
+    public class YandexProfileTests
+    {
+        private IMapper _target;
+
+        private Fixture _fixture;
+
+        [SetUp]
+        public void InitTest()
+        {
+            _target = new Mapper(new MapperConfiguration(c => c.AddProfile<MarusiaProfile>()));
+
+            _fixture = new Fixture();
+            _fixture.Customizations.Add(new TypeRelay(typeof(Button), typeof(ResponseButton)));
+            _fixture.Customizations.Add(new TypeRelay(typeof(ICard), typeof(ItemsListCard)));
+        }
+
+        [Test]
+        public void ValidateConfiguration_Success()
+        {
+            _target.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+
+        [Test]
+        public void Map_InputToOutput()
+        {
+            var input = _fixture.Create<InputModel>();
+
+            var output = _fixture.Build<OutputModel>()
+                .Without(o => o.Session)
+                .Without(o => o.Version)
+                .Create();
+
+
+            _target.Map(input, output);
+
+
+            Assert.AreEqual(input.Session.SessionId, output.Session.SessionId);
+            Assert.AreEqual(input.Session.MessageId, output.Session.MessageId);
+            Assert.AreEqual(input.Version, output.Version);
+            Assert.NotNull(output.Response);
+        }
+    }
+}
---
+++ src/GranSteL.Chatbot.Messengers.Marusia.Tests/MarusiaServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading.Tasks;
+using AutoFixture;
+using AutoMapper;
+using GranSteL.Chatbot.Services;
+using Moq;
+using NUnit.Framework;
+using Request = GranSteL.Chatbot.Models.Request;
+using Response = GranSteL.Chatbot.Models.Response;
+
+namespace GranSteL.Chatbot.Messengers.Marusia.Tests
+{
+    [TestFixture]
+    public class YandexServiceTests
+    {
+        private MockRepository _mockRepository;
+
+        private Mock<IConversationService> _conversationService;
+        private Mock<IMapper> _mapper;
+
+        private YandexService _target;
+
+        private Fixture _fixture;
+
+        [SetUp]
+        public void InitTest()
+        {
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+
+            _conversationService = _mockRepository.Create<IConversationService>();
+            _mapper = _mockRepository.Create<IMapper>();
+
+            _target = new YandexService(_conversationService.Object, _mapper.Object);
+
+            _fixture = new Fixture();
+        }
+
+        [Test]
+        public async Task ProcessIncomingAsync_Invokations_Success()
+        {
+            var inputModel = _fixture.Build<InputModel>()
+                .OmitAutoProperties()
+                .Create();
+
+            var request = _fixture.Build<Request>()
+                .OmitAutoProperties()
+                .Create();
+
+            _mapper.Setup(m => m.Map<Request>(It.IsAny<InputModel>())).Returns(request);
+
+            _conversationService.Setup(s => s.GetResponseAsync(request)).ReturnsAsync(() => null);
+
+            _mapper.Setup(m => m.Map(It.IsAny<Request>(), It.IsAny<Response>())).Returns(() => null);
+
+            var output = _fixture.Build<OutputModel>()
+                .With(o => o.Session)
+                .OmitAutoProperties()
+                .Create();
+
+            _mapper.Setup(m => m.Map<OutputModel>(It.IsAny<Response>())).Returns(output);
+            _mapper.Setup(m => m.Map(It.IsAny<InputModel>(), It.IsAny<OutputModel>())).Returns(() => null);
+
+
+            var result = await _target.ProcessIncomingAsync(inputModel);
+
+
+            _mockRepository.VerifyAll();
+
+            Assert.NotNull(result);
+        }
+    }
+}
