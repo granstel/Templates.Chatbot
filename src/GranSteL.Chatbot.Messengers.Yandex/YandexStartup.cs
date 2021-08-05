0b98ae5bf962218d49d857cc3ff17259ac6e21ec
Stepan Grankin <sgrankin@inbox.ru>
2021-08-05 09:42:02 +0300

    replace YandexDependencyModule with YandexStartup

---
+++ src/GranSteL.Chatbot.Messengers.Yandex/YandexStartup.cs
@@ -0,0 +1,20 @@
+﻿using GranSteL.Chatbot.Messengers.Yandex;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(YandexStartup))]
+namespace GranSteL.Chatbot.Messengers.Yandex
+{
+    public class YandexStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddConfiguration<YandexConfiguration>("appsettings.Yandex.json");
+
+                services.AddTransient<IYandexService, YandexService>();
+            });
+        }
+    }
+}
