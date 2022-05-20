ca7892fad819cfcc87529953703b152bef26b494
Stepan Grankin <sgrankin@inbox.ru>
2022-05-20 11:49:41 +0300

    Chat2Desk files removed

--- src/AddChat2Desk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-dotnet sln add GranSteL.Chatbot.Messengers.Chat2Desk/GranSteL.Chatbot.Messengers.Chat2Desk.csproj -s Messengers
-dotnet sln add GranSteL.Chatbot.Messengers.Chat2Desk.Tests/GranSteL.Chatbot.Messengers.Chat2Desk.Tests.csproj -s Tests
-dotnet add GranSteL.Chatbot.Api/GranSteL.Chatbot.Api.csproj reference GranSteL.Chatbot.Messengers.Chat2Desk/GranSteL.Chatbot.Messengers.Chat2Desk.csproj
